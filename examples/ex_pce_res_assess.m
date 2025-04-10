5b28a138f435069b1012e90456d8d2b220980941
AGerkis <a.gerkis1@gmail.com>
2025-04-10 17:20:20 -0400

    Added an example, computing PCE models using the MmLHS method to assess resilience in the 39-Bus system.

---
+++ examples/ex_pce_res_assess.m
@@ -0,0 +1,80 @@
+% ex_pce_res_assess.m
+%
+% An example highlighting how PSRA-PCE can be applied to assess power
+% system resilience. Computes a PCE model of the Phi_LS resilience metric
+% in the 39-Bus system.
+%
+% Author: Aidan Gerkis
+% Date: 10-04-2025
+
+clear; close all; clc;
+uqlab;
+
+%% Specify Simulation Options
+sim_opt = struct(); % Create empty structure
+
+% Simulation Parameters
+sim_opt.n_s = 10; % Number of model evaluations to perform in MCS
+sim_opt.n_r = 1; % Number of replications (useful for stochastic models)
+sim_opt.n_pool = 14; % Number of parallel pools to use (IMPORTANT: SHOULD BE LESS THAN NUMBER OF AVAILABLE CORES, RUN feature('numcores') to see this value)
+sim_opt.plotting = 1; % Make plots visualizing MCS results
+
+% Experiment Saving Parameters (Setting outname to an empty CHAR array disables saving)
+sim_opt.savdir = ""; % <------------------- Set this to a convenient location!
+sim_opt.outname = ''; % Output filename
+
+% Model Parameters
+sim_opt.n_in = 12; % Number of model inputs (2*Size of Active Set)
+sim_opt.n_out = 10; % Computing all metrics for 2 indicators
+
+% Specify Inputs
+load("example_input_39bus.mat");
+sim_opt.input = uq_createInput(input.Options);
+
+% Specify Model
+Params = ps_resilience_params("39bus_exp"); % Get default parameters
+Params.output = [1 1]; % Compute only Phi_LS metric
+model_opts.mFile = 'uq_psres';
+model_opts.Parameters = Params;
+
+sim_opt.model = uq_createModel(model_opts); % Create Model
+
+%% Specify Experiment Design Function
+% Define options
+exp_opt = struct();
+exp_opt.Method = 'Maximin'; % Specify Maximin design method
+exp_opt.Distance = 'Euclidean'; % Use Euclidean distance to compute Maximin distances
+exp_opt.N_cand = 50; % Number of candidate designs for MmLHS
+
+% Create function handle
+design_exp = @(N)uq_design_exp(sim_opt.input, N, 1, maximin_opts);
+
+%% Generate experiments
+exp = gen_exp(sim_opt, design_exp);
+
+%% Compute PCE Models
+% Here PCE models are computed for the system response in the disturbance
+% stage. For this reason the recovery time inputs (in array positions 7:12)
+% are ignored.
+
+% Define PCE Model Options
+PCEOpts.Type = 'Metamodel';
+PCEOpts.MetaType = 'PCE';
+PCEOpts.Method = 'LARS'; % Coefficient & Basis Computation Method
+PCEOpts.ExpDesign.Sampling = 'user'; % Indicate that an experiment has already been computed
+PCEOpts.Degree = 3:12; % Maximum polynomial Degree to Use
+PCEOpts.TruncOptions.qNorm = [1, 0.75, 0.5, 0.25, 0.1]; % Trunction Norm order - Use default to start
+PCEOpts.DegreeEarlyStop = true;
+PCEOpts.LARS.LarsEarlyStop = true;
+PCEOpts.LARS.ModifiedLoo = true;
+PCEOpts.Input = input; % Assign Input
+[PCEOpts.PolyTypes{1:6}] = deal('arbitrary'); % Set basis type
+
+% Assign experiment to PCE Options structures
+PCEOpts.ExpDesign.X = exp.in(:, 1:6); % Assign inputs
+PCEOpts.ExpDesign.Y = exp.out(:, 1:6); % Assign corresponding outputs
+
+% Compute PCE Model
+pce_model = uq_createModel(PCEOpts);
+
+%% Assess Resilience
